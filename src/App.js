819a2d174e9c262759a6f3d81d744af38cc62c8c
Spencer Reeves <spencer.ed.reeves@gmail.com>
2019-09-08 18:25:07 -0700

    Basic setup with title and subtitle (with rotation)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Title, SubTitle, Header} from './Home.js';
 
 function App() {
   return (
-    <div className="App background-twilight">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="background-twilight full-screen">
+      <div id="body" className="padding-40">
+        <Header/>
+        <Title/>
+        <SubTitle />
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect}from 'react';
+import './Home.css';
+
+export const Title = () => {
+  return (
+    <div className='top-30'>
+      <h1 className=" HomeTitle homeText">REEVES</h1>
+    </div>
+  );
+};
+
+export const SubTitle = () => {
+  const titles = ['Software Consultant', 'UI/UX Designer', 'Product Owner', 'Product Manager', 'Software Engineer'];
+  const [index, setIndex] = useState(0);
+
+  // Iterate through the titles every 1.5s
+  useEffect( () => {
+    const interval = setInterval( () => {
+      if (index < titles.length - 1) {
+        setIndex(index => index + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1500);
+    // Clear the interval if user navigates away from page
+    return () => clearInterval(interval);
+
+    // Add dependencies used in Effect
+  }, [titles, index, setIndex]);
+
+  return (
+    <div>
+      <h4 className='HomeSubTitle homeText'>-&nbsp;{titles[index]}&nbsp;-</h4>
+    </div>
+  );
+};
+
+export const Header = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+
