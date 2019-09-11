47ca7500f8f1f5e5917dc6b8d1f88c03dd384938
Spencer Reeves <spencer.ed.reeves@gmail.com>
2019-09-11 09:45:52 -0700

    Added scroll bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Title, SubTitle, Menu} from './Home.js';
+import {ScrollBar} from "./ScrollBar";
 
 function App() {
   return (
     <div id="app" className="background-twilight full-screen">
       <div id="body">
         <Menu/>
+        <ScrollBar/>
         <Title/>
         <SubTitle />
       </div>
---
+++ src/ScrollBar.js
@@ -0,0 +1,27 @@
+import React, { useReducer}from 'react';
+import './ScrollBar.css';
+
+const heightReducer= (state, action) => {
+  console.log(state, action);
+  if (action && action < 4) {
+    return {top: (action * 25).toString() + '%'};
+  } else {
+    return {top: '0%'};
+  }
+};
+
+export const ScrollBar = () => {
+  const [height, heightDispatch] = useReducer(heightReducer, {top: '0%'});
+  console.log(height);
+
+  return (
+    <div className='ScrollBar'>
+      <div className='ScrollBarIndicator' style={height}/>
+      <div className='ScrollSection' onClick={() => heightDispatch(0)}/>
+      <div className='ScrollSection' onClick={() => heightDispatch(1)}/>
+      <div className='ScrollSection' onClick={() => heightDispatch(2)}/>
+      <div className='ScrollSection' onClick={() => heightDispatch(3)}/>
+    </div>
+  );
+};
+
